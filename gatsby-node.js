0c1d65aa8a7f9fd1e811652d840da2d2648d984d
Jakob  Elben <jakobelben@Jakobs-MacBook-Pro.local>
2019-06-02 15:47:25 +0200

    Update

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,50 +37,74 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     }
 }
 
-exports.createPages = ({ graphql, actions }) => {
-    doc.notebook.forEach(x1 => {
-        x1.notebooks.forEach(x2 => {
-            fetch("https://raw.githubusercontent.com/" + x2.owner + "/" + x2.repository + "/master/" + x2.path).then(reponse => reponse.json()).then(data => {
-                actions.createPage({
-                    path: x1.path + baseName(x2.path),
-                    component: require.resolve("./src/templates/Notebook.jsx"),
-                    context: {
-                        data: data
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    return new Promise((resolve, reject) => {
+        const blogPostTemplate = require.resolve("./src/templates/Markdown.jsx")
+        const NotebookTemplate = require.resolve("./src/templates/Notebook.jsx")
+        // Query for markdown nodes to use in creating pages.
+        resolve(
+            graphql(
+                `
+                {
+                    allMarkdownRemark {
+                      edges {
+                        node {
+                          fields {
+                            slug
+                          }
+                        }
+                      }
                     }
+                  }
+          `
+            ).then(async result => {
+                if (result.errors) {
+                    reject(result.errors)
+                }
+
+                // Create pages for each markdown file.
+                result.data.allMarkdownRemark.edges.forEach(async ({ node }) => {
+                    createPage({
+                        path: node.fields.slug,
+                        component: blogPostTemplate,
+                        // In your blog post template's graphql query, you can use path
+                        // as a GraphQL variable to query for data from the markdown file.
+                        context: {
+                            slug: node.fields.slug,
+                        },
+                    })
                 })
-            })
-        })
-    })
-    graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `).then(result => {
-        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-            actions.createPage({
-                path: node.fields.slug,
-                component: require.resolve("./src/templates/Markdown.jsx"),
-                context: {
-                    // Data passed to context is available
-                    // in page queries as GraphQL variables.
-                    slug: node.fields.slug,
-                },
-            })
-        })
-    })
-}
+                // for (x1 in doc.notebook) {
+                //     for (x2 in x1.notebooks) {
+                //         const notebook = () => axios.get("https://raw.githubusercontent.com/NumEconCopenhagen/lectures-2019/master/02/Primitives.ipynb");
+                //         const responseLanguages = await fetchLanguages();
+                //         createPage({
+                //             path: baseName,
+                //             component: NotebookTemplate,
+                //             context: {
+                //                 data: responseLanguages.data
+                //             }
+                //         })
+                //     }
+                // }
+                for (x1 in doc.notebook) {
+                    for (x2 in doc.notebook[x1].notebooks) {
+                        const url = "https://raw.githubusercontent.com/" + doc.notebook[x1].notebooks[x2].owner + "/" + doc.notebook[x1].notebooks[x2].repository + "/master/" + doc.notebook[x1].notebooks[x2].path
+                        const fetchNotebook = () => axios.get(url);
+                        const responseNotebook = await fetchNotebook();
+                        createPage({
+                            path: doc.notebook[x1].path + baseName(doc.notebook[x1].notebooks[x2].path),
+                            component: NotebookTemplate,
+                            context: {
+                                data: responseNotebook.data
+                            }
+                        })
+                    }
+                }
 
-
-exports.onCreateWebpackConfig = ({ actions }) => {
-    actions.setWebpackConfig({
-        node: { fs: 'empty' },
+            })
+        )
     })
 }
